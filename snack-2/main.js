df0beac74908d1b29b4146d771557d584d7d7d3c
SimoNapo02 <simonematteonapolitano@gmail.com>
2022-05-05 16:31:02 +0200

    js

--- snack-2/main.js
+++ snack-2/main.js
@@ -1,9 +1,18 @@
 // Fai inserire un numero all'utente
 let numero;
 do{
-    numero = Number(prompt("inserisci un numero di array da generare"))
+    numero = Number(prompt("quanti array si vogliono generare?"))
 }
 
 // Genera N array, ognuno formato da 10 numeri casuali tra 1 e 100.
-
 // Ogni volta che ne crei uno, stampalo a schermo
+while(isNaN(numero))
+
+do{   
+    const array = []
+    for(let i = 0 ; i<10; i++){
+        array.push(Math.floor(Math.random()*100 +1))
+    }
+    console.log(array)
+    numero--;
+}while(numero>0)
